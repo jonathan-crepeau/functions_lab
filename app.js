a35add4264a4962e84f9081766454f66c562fc98
jonathan-crepeau <jon.crepeau@gmail.com>
2024-02-15 12:22:19 -0800

    function lab exercises #1-2 completed

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+
+// ANCHOR - Functions Lab Exercises
+
+// SECTION Exercise #1: Write a function, 'printGreeting', with a parameter, 'name', that returns a greeting with the argument interpolated into the greeting:
+
+// const printGreeting = (name) => {
+//   console.log('Morning ' + name + '!');
+// }
+// printGreeting('Luna');
+
+
+// SECTION Exercise #2: Write a function, 'reverseWordOrder' that accepts a single string as a parameter. The function should return a string with the order of the words reversed. Don't worry about punctuation.
+
+// const reverseWordOrder = (inputString) => {
+//   return inputString.split(' ').reverse().join(' ');
+// }
+// console.log(reverseWordOrder('My name is Jonathan and I am a ginger'));
+
+
+// SECTION Exercise #3:
